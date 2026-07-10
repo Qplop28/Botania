3255197a8c5f1cdb056d0c06303ca2df4e24f80c
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-10 16:42:36 -0700

    refactor(minecraft): migrate custom particle serialization

--- Xplat/src/main/java/vazkii/botania/client/fx/WispParticleData.java
+++ Xplat/src/main/java/vazkii/botania/client/fx/WispParticleData.java
@@ -8,22 +8,18 @@
  */
 package vazkii.botania.client.fx;
 
-import com.mojang.brigadier.StringReader;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.mojang.serialization.MapCodec;
 
 import net.minecraft.core.particles.ParticleOptions;
 import net.minecraft.core.particles.ParticleType;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.FriendlyByteBuf;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.util.Locale;
-
 public class WispParticleData implements ParticleOptions {
-	public static final Codec<WispParticleData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+	public static final MapCodec<WispParticleData> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
 			Codec.FLOAT.fieldOf("size").forGetter(d -> d.size),
 			Codec.FLOAT.fieldOf("r").forGetter(d -> d.r),
 			Codec.FLOAT.fieldOf("g").forGetter(d -> d.g),
@@ -33,7 +29,11 @@ public class WispParticleData implements ParticleOptions {
 			Codec.BOOL.fieldOf("noClip").forGetter(d -> d.noClip),
 			Codec.FLOAT.optionalFieldOf("gravity", 0.0f).forGetter(d -> d.gravity)
 	)
-			.apply(instance, WispParticleData::new));
+
+	public static final StreamCodec<FriendlyByteBuf, WispParticleData> STREAM_CODEC =
+		StreamCodec.ofMember(WispParticleData::writeToNetwork, WispParticleData::fromNetwork);
+
+	.apply(instance, WispParticleData::new));
 	public final float size;
 	public final float r, g, b;
 	public final float maxAgeMul;
@@ -91,59 +91,27 @@ public class WispParticleData implements ParticleOptions {
 	}
 
 	@Override
-	public void writeToNetwork(FriendlyByteBuf buf) {
-		buf.writeFloat(size);
-		buf.writeFloat(r);
-		buf.writeFloat(g);
-		buf.writeFloat(b);
-		buf.writeFloat(maxAgeMul);
-		buf.writeBoolean(depthTest);
-		buf.writeBoolean(noClip);
-		buf.writeFloat(gravity);
+	private void writeToNetwork(FriendlyByteBuf buf) {
+			buf.writeFloat(size);
+			buf.writeFloat(r);
+			buf.writeFloat(g);
+			buf.writeFloat(b);
+			buf.writeFloat(maxAgeMul);
+			buf.writeBoolean(depthTest);
+			buf.writeBoolean(noClip);
+			buf.writeFloat(gravity);
 	}
 
-	@NotNull
-	@Override
-	public String writeToString() {
-		return String.format(Locale.ROOT, "%s %.2f %.2f %.2f %.2f %.2f %s",
-				BuiltInRegistries.PARTICLE_TYPE.getKey(getType()), this.size, this.r, this.g, this.b, this.maxAgeMul, this.depthTest);
+	private static WispParticleData fromNetwork(FriendlyByteBuf buf) {
+		return new WispParticleData(
+			buf.readFloat(),
+			buf.readFloat(),
+			buf.readFloat(),
+			buf.readFloat(),
+			buf.readFloat(),
+			buf.readBoolean(),
+			buf.readBoolean(),
+			buf.readFloat()
+		);
 	}
-
-	public static final Deserializer<WispParticleData> DESERIALIZER = new Deserializer<>() {
-		@NotNull
-		@Override
-		public WispParticleData fromCommand(@NotNull ParticleType<WispParticleData> type, @NotNull StringReader reader) throws CommandSyntaxException {
-			reader.expect(' ');
-			float size = reader.readFloat();
-			reader.expect(' ');
-			float r = reader.readFloat();
-			reader.expect(' ');
-			float g = reader.readFloat();
-			reader.expect(' ');
-			float b = reader.readFloat();
-			reader.expect(' ');
-			float mam = reader.readFloat();
-			boolean depth = true;
-			if (reader.canRead()) {
-				reader.expect(' ');
-				depth = reader.readBoolean();
-			}
-			boolean noClip = false;
-			if (reader.canRead()) {
-				reader.expect(' ');
-				noClip = reader.readBoolean();
-			}
-			float gravity = 0;
-			if (reader.canRead()) {
-				reader.expect(' ');
-				gravity = reader.readFloat();
-			}
-			return new WispParticleData(size, r, g, b, mam, depth, noClip, gravity);
-		}
-
-		@Override
-		public WispParticleData fromNetwork(@NotNull ParticleType<WispParticleData> type, FriendlyByteBuf buf) {
-			return new WispParticleData(buf.readFloat(), buf.readFloat(), buf.readFloat(), buf.readFloat(), buf.readFloat(), buf.readBoolean(), buf.readBoolean(), buf.readFloat());
-		}
-	};
 }

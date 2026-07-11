dcbdf3363b26cc12c211bcf764e63b874224d9a5
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-11 00:27:30 -0700

    refactor(minecraft): migrate axe tool material

--- Xplat/src/main/java/vazkii/botania/common/item/equipment/tool/manasteel/ManasteelAxeItem.java
+++ Xplat/src/main/java/vazkii/botania/common/item/equipment/tool/manasteel/ManasteelAxeItem.java
@@ -16,7 +16,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.AxeItem;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Tier;
+import net.minecraft.world.item.ToolMaterial;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -44,7 +44,7 @@ public class ManasteelAxeItem extends AxeItem implements CustomDamageItem, Sorta
 		this(BotaniaAPI.instance().getManasteelItemTier(), 6F, -3.1F, props);
 	}
 
-	public ManasteelAxeItem(Tier mat, float attackDamage, float attackSpeed, Properties props) {
+	public ManasteelAxeItem(ToolMaterial mat, float attackDamage, float attackSpeed, Properties props) {
 		super(mat, attackDamage, attackSpeed, props);
 	}
 

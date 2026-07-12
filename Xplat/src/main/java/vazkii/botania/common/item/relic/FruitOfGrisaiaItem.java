ee3f06c799b74309c5c54623aeb87eab84fa2dc2
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-11 18:03:05 -0700

    refactor(minecraft): migrate Fruit of Grisaia item use

--- Xplat/src/main/java/vazkii/botania/common/item/relic/FruitOfGrisaiaItem.java
+++ Xplat/src/main/java/vazkii/botania/common/item/relic/FruitOfGrisaiaItem.java
@@ -9,12 +9,12 @@
 package vazkii.botania.common.item.relic;
 
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUtils;
-import net.minecraft.world.item.UseAnim;
+import net.minecraft.world.item.ItemUseAnimation;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 
@@ -36,25 +36,25 @@ public class FruitOfGrisaiaItem extends RelicItem {
 	}
 
 	@Override
-	public int getUseDuration(ItemStack stack) {
+	public int getUseDuration(ItemStack stack, LivingEntity user) {
 		return 32;
 	}
 
 	@NotNull
 	@Override
-	public UseAnim getUseAnimation(ItemStack stack) {
-		return isBoot(stack) ? UseAnim.DRINK : UseAnim.EAT;
+	public ItemUseAnimation getUseAnimation(ItemStack stack) {
+		return isBoot(stack) ? ItemUseAnimation.DRINK : ItemUseAnimation.EAT;
 	}
 
 	@NotNull
 	@Override
-	public InteractionResultHolder<ItemStack> use(Level world, Player player, @NotNull InteractionHand hand) {
+	public InteractionResult use(Level world, Player player, @NotNull InteractionHand hand) {
 		ItemStack stack = player.getItemInHand(hand);
 		var relic = XplatAbstractions.INSTANCE.findRelic(stack);
 		if (player.canEat(false) && relic != null && relic.isRightPlayer(player)) {
 			return ItemUtils.startUsingInstantly(world, player, hand);
 		}
-		return InteractionResultHolder.pass(stack);
+		return InteractionResult.PASS;
 	}
 
 	@Override

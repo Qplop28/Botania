18ff9c355b9fcdb8741011f5787862ea96f685a8
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-12 18:42:45 -0700

    fix(minecraft): migrate mana pool minecart contracts

--- Xplat/src/main/java/vazkii/botania/common/entity/ManaPoolMinecartEntity.java
+++ Xplat/src/main/java/vazkii/botania/common/entity/ManaPoolMinecartEntity.java
@@ -10,10 +10,10 @@ package vazkii.botania.common.entity;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.syncher.EntityDataAccessor;
 import net.minecraft.network.syncher.EntityDataSerializers;
 import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
@@ -21,8 +21,12 @@ import net.minecraft.world.entity.vehicle.minecart.AbstractMinecart;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseRailBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
+import net.minecraft.world.phys.Vec3;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -35,52 +39,82 @@ import vazkii.botania.common.block.block_entity.mana.ManaPumpBlockEntity;
 import vazkii.botania.common.item.BotaniaItems;
 import vazkii.botania.xplat.XplatAbstractions;
 
-public class ManaPoolMinecartEntity extends AbstractMinecart {
+public class ManaPoolMinecartEntity
+		extends AbstractMinecart {
 	private static final int TRANSFER_RATE = 10000;
 	private static final String TAG_MANA = "mana";
-	private static final EntityDataAccessor<Integer> MANA = SynchedEntityData.defineId(ManaPoolMinecartEntity.class, EntityDataSerializers.INT);
 
-	public ManaPoolMinecartEntity(EntityType<ManaPoolMinecartEntity> type, Level world) {
+	private static final EntityDataAccessor<Integer> MANA =
+			SynchedEntityData.defineId(
+					ManaPoolMinecartEntity.class,
+					EntityDataSerializers.INT
+			);
+
+	public ManaPoolMinecartEntity(
+			EntityType<ManaPoolMinecartEntity> type,
+			Level world) {
 		super(type, world);
 	}
 
-	public ManaPoolMinecartEntity(Level world, double x, double y, double z) {
-		super(BotaniaEntities.POOL_MINECART, world, x, y, z);
+	public ManaPoolMinecartEntity(
+			Level world,
+			double x,
+			double y,
+			double z) {
+		super(
+				BotaniaEntities.POOL_MINECART,
+				world,
+				x,
+				y,
+				z
+		);
 	}
 
 	@Override
-	protected void defineSynchedData() {
-		super.defineSynchedData();
+	protected void defineSynchedData(
+			SynchedEntityData.Builder entityData) {
+		super.defineSynchedData(entityData);
 		entityData.define(MANA, 0);
 	}
 
 	@NotNull
 	@Override
 	public BlockState getDisplayBlockState() {
-		return BotaniaBlocks.manaPool.defaultBlockState();
+		return BotaniaBlocks.manaPool
+				.defaultBlockState();
 	}
 
-	@NotNull
+	/*
+	 * AbstractMinecart.Type was removed. This replaces
+	 * the former Type.RIDEABLE classification.
+	 */
 	@Override
-	public AbstractMinecart.Type getMinecartType() {
-		return Type.RIDEABLE;
+	public boolean isRideable() {
+		return true;
 	}
 
 	@Override
-	protected boolean canAddPassenger(Entity passenger) {
+	protected boolean canAddPassenger(
+			Entity passenger) {
 		return false;
 	}
 
 	@Override
-	protected void applyNaturalSlowdown() {
-		float f = 0.98F;
-		this.setDeltaMovement(getDeltaMovement().multiply(f, 0, f));
+	protected Vec3 applyNaturalSlowdown(
+			Vec3 movement) {
+		return movement.multiply(
+				0.98,
+				0.0,
+				0.98
+		);
 	}
 
 	@NotNull
 	@Override
 	public ItemStack getPickResult() {
-		return new ItemStack(BotaniaItems.poolMinecart);
+		return new ItemStack(
+				BotaniaItems.poolMinecart
+		);
 	}
 
 	@Override
@@ -90,104 +124,233 @@ public class ManaPoolMinecartEntity extends AbstractMinecart {
 
 	@Override
 	public void tick() {
+		if (level() instanceof ServerLevel serverLevel) {
+			/*
+			 * Capture the rail position before movement.
+			 * The old moveAlongTrack override received
+			 * this position before calling its superclass.
+			 */
+			BlockPos railPos =
+					getCurrentBlockPosOrRailBelow();
+
+			boolean onRail =
+					BaseRailBlock.isRail(
+							serverLevel.getBlockState(
+									railPos
+							)
+					);
+
+			super.tick();
+
+			if (onRail && !isRemoved()) {
+				transferMana(
+						serverLevel,
+						railPos
+				);
+			}
+
+			return;
+		}
+
 		super.tick();
 
-		if (level().isClientSide) {
-			double particleChance = 1F - (double) getMana() / (double) ManaPoolBlockEntity.MAX_MANA * 0.1;
-			int color = ManaPoolBlockEntity.PARTICLE_COLOR;
-			float red = (color >> 16 & 0xFF) / 255F;
-			float green = (color >> 8 & 0xFF) / 255F;
-			float blue = (color & 0xFF) / 255F;
-			double x = Mth.floor(getX());
-			double y = Mth.floor(getY());
-			double z = Mth.floor(getZ());
-			if (Math.random() > particleChance) {
-				WispParticleData data = WispParticleData.wisp((float) Math.random() / 3F, red, green, blue, 2F);
-				level().addParticle(data, x + 0.3 + Math.random() * 0.5, y + 0.85 + Math.random() * 0.25, z + Math.random(), 0, (float) Math.random() / 25F, 0);
-			}
+		double particleChance =
+				1.0
+						- (double) getMana()
+						/ ManaPoolBlockEntity.MAX_MANA
+						* 0.1;
+
+		int color =
+				ManaPoolBlockEntity.PARTICLE_COLOR;
+
+		float red =
+				(color >> 16 & 0xFF) / 255.0F;
+
+		float green =
+				(color >> 8 & 0xFF) / 255.0F;
+
+		float blue =
+				(color & 0xFF) / 255.0F;
+
+		double x = Mth.floor(getX());
+		double y = Mth.floor(getY());
+		double z = Mth.floor(getZ());
+
+		if (Math.random() > particleChance) {
+			WispParticleData data =
+					WispParticleData.wisp(
+							(float) Math.random()
+									/ 3.0F,
+							red,
+							green,
+							blue,
+							2.0F
+					);
+
+			level().addParticle(
+					data,
+					x + 0.3
+							+ Math.random() * 0.5,
+					y + 0.85
+							+ Math.random() * 0.25,
+					z + Math.random(),
+					0.0,
+					(float) Math.random()
+							/ 25.0F,
+					0.0
+			);
 		}
 	}
 
-	@Override
-	public void moveAlongTrack(BlockPos pos, BlockState state) {
-		super.moveAlongTrack(pos, state);
+	private void transferMana(
+			ServerLevel level,
+			BlockPos railPos) {
+		for (Direction direction
+				: Direction.Plane.HORIZONTAL) {
+			BlockPos pumpPos =
+					railPos.relative(direction);
 
-		for (Direction dir : Direction.Plane.HORIZONTAL) {
-			BlockPos pumpPos = pos.relative(dir);
-			BlockState pumpState = level().getBlockState(pumpPos);
-			if (pumpState.is(BotaniaBlocks.pump)) {
-				ManaPumpBlockEntity pump = (ManaPumpBlockEntity) level().getBlockEntity(pumpPos);
-				BlockPos poolPos = pumpPos.relative(dir);
-				var receiver = XplatAbstractions.INSTANCE.findManaReceiver(level(), poolPos, dir.getOpposite());
+			BlockState pumpState =
+					level.getBlockState(pumpPos);
 
-				if (receiver instanceof ManaPool pool) {
-					Direction pumpDir = pumpState.getValue(BlockStateProperties.HORIZONTAL_FACING);
-					boolean did = false;
-					boolean can = false;
+			if (!pumpState.is(BotaniaBlocks.pump)) {
+				continue;
+			}
 
-					if (pumpDir == dir) { // Pool -> Cart
-						can = true;
+			if (!(level.getBlockEntity(pumpPos)
+					instanceof ManaPumpBlockEntity pump)) {
+				continue;
+			}
 
-						if (!pump.hasRedstone) {
-							int cartMana = getMana();
-							int poolMana = pool.getCurrentMana();
-							int transfer = Math.min(TRANSFER_RATE, poolMana);
-							int actualTransfer = Math.min(ManaPoolBlockEntity.MAX_MANA - cartMana, transfer);
-							if (actualTransfer > 0) {
-								pool.receiveMana(-transfer);
-								setMana(cartMana + actualTransfer);
-								did = true;
-							}
-						}
-					} else if (pumpDir == dir.getOpposite()) { // Cart -> Pool
-						can = true;
+			BlockPos poolPos =
+					pumpPos.relative(direction);
 
-						if (!pump.hasRedstone && !pool.isFull()) {
-							int cartMana = getMana();
-							int transfer = Math.min(TRANSFER_RATE, cartMana);
-							if (transfer > 0) {
-								pool.receiveMana(transfer);
-								setMana(cartMana - transfer);
-								did = true;
-							}
-						}
+			var receiver =
+					XplatAbstractions.INSTANCE
+							.findManaReceiver(
+									level,
+									poolPos,
+									direction
+											.getOpposite()
+							);
+
+			if (!(receiver instanceof ManaPool pool)) {
+				continue;
+			}
+
+			Direction pumpDirection =
+					pumpState.getValue(
+							BlockStateProperties
+									.HORIZONTAL_FACING
+					);
+
+			boolean transferred = false;
+			boolean validDirection = false;
+
+			if (pumpDirection == direction) {
+				// Pool -> cart
+				validDirection = true;
+
+				if (!pump.hasRedstone) {
+					int cartMana = getMana();
+					int poolMana =
+							pool.getCurrentMana();
+
+					int transfer =
+							Math.min(
+									TRANSFER_RATE,
+									poolMana
+							);
+
+					int actualTransfer =
+							Math.min(
+									ManaPoolBlockEntity
+											.MAX_MANA
+											- cartMana,
+									transfer
+							);
+
+					if (actualTransfer > 0) {
+						pool.receiveMana(-transfer);
+
+						setMana(
+								cartMana
+										+ actualTransfer
+						);
+
+						transferred = true;
 					}
+				}
+			} else if (
+					pumpDirection
+							== direction.getOpposite()
+			) {
+				// Cart -> pool
+				validDirection = true;
 
-					if (did) {
-						pump.hasCart = true;
-						pump.setActive(true);
+				if (!pump.hasRedstone
+						&& !pool.isFull()) {
+					int cartMana = getMana();
+
+					int transfer =
+							Math.min(
+									TRANSFER_RATE,
+									cartMana
+							);
+
+					if (transfer > 0) {
+						pool.receiveMana(transfer);
+						setMana(cartMana - transfer);
+						transferred = true;
 					}
-
-					if (can) {
-						pump.hasCartOnTop = true;
-						pump.comparator = (int) ((double) getMana() / (double) ManaPoolBlockEntity.MAX_MANA * 15); // different from ManaPoolBlockEntity.calculateComparatorLevel, kept for compatibility
-					}
-
 				}
 			}
+
+			if (transferred) {
+				pump.hasCart = true;
+				pump.setActive(true);
+			}
+
+			if (validDirection) {
+				pump.hasCartOnTop = true;
+
+				pump.comparator =
+						(int) (
+								(double) getMana()
+										/ ManaPoolBlockEntity
+												.MAX_MANA
+										* 15.0
+						);
+			}
 		}
 	}
 
 	@Override
-	protected void addAdditionalSaveData(@NotNull CompoundTag cmp) {
-		super.addAdditionalSaveData(cmp);
-		cmp.putInt(TAG_MANA, getMana());
+	protected void addAdditionalSaveData(
+			ValueOutput output) {
+		super.addAdditionalSaveData(output);
+		output.putInt(TAG_MANA, getMana());
 	}
 
 	@Override
-	protected void readAdditionalSaveData(CompoundTag cmp) {
-		super.readAdditionalSaveData(cmp);
-		setMana(cmp.getInt(TAG_MANA));
+	protected void readAdditionalSaveData(
+			ValueInput input) {
+		super.readAdditionalSaveData(input);
+		setMana(input.getIntOr(TAG_MANA, 0));
 	}
 
 	@Override
-	public Item getDropItem() {
+	protected Item getDropItem() {
 		return BotaniaItems.poolMinecart;
 	}
 
 	@SoftImplement("IForgeMinecart")
 	public int getComparatorLevel() {
-		return ManaPoolBlockEntity.calculateComparatorLevel(getMana(), ManaPoolBlockEntity.MAX_MANA);
+		return ManaPoolBlockEntity
+				.calculateComparatorLevel(
+						getMana(),
+						ManaPoolBlockEntity.MAX_MANA
+				);
 	}
 
 	public int getMana() {
@@ -197,5 +360,4 @@ public class ManaPoolMinecartEntity extends AbstractMinecart {
 	public void setMana(int mana) {
 		entityData.set(MANA, mana);
 	}
-
 }

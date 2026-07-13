4673ec8b909734563b38a79792d5b37f2d3284e0
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-13 00:00:24 -0700

    fix(minecraft): migrate wand dispenser source

--- Xplat/src/main/java/vazkii/botania/common/block/dispenser/WandBehavior.java
+++ Xplat/src/main/java/vazkii/botania/common/block/dispenser/WandBehavior.java
@@ -9,8 +9,8 @@
 package vazkii.botania.common.block.dispenser;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.BlockSource;
 import net.minecraft.core.Direction;
+import net.minecraft.core.dispenser.BlockSource;
 import net.minecraft.core.dispenser.OptionalDispenseItemBehavior;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -22,18 +22,43 @@ import org.jetbrains.annotations.NotNull;
 import vazkii.botania.api.block.Wandable;
 import vazkii.botania.xplat.XplatAbstractions;
 
-public class WandBehavior extends OptionalDispenseItemBehavior {
-
+public class WandBehavior
+		extends OptionalDispenseItemBehavior {
 	@NotNull
 	@Override
-	protected ItemStack execute(BlockSource source, ItemStack stack) {
-		Level world = source.getLevel();
-		Direction facing = source.getBlockState().getValue(DispenserBlock.FACING);
-		BlockPos pos = source.getPos().relative(facing);
-		BlockState state = world.getBlockState(pos);
-		Wandable wandable = XplatAbstractions.INSTANCE.findWandable(world, pos, state, world.getBlockEntity(pos));
-		setSuccess(wandable != null && wandable.onUsedByWand(null, stack, facing.getOpposite()));
+	protected ItemStack execute(
+			BlockSource source,
+			ItemStack stack) {
+		Level level = source.level();
+
+		Direction facing =
+				source.state().getValue(
+						DispenserBlock.FACING
+				);
+
+		BlockPos target =
+				source.pos().relative(facing);
+
+		BlockState state =
+				level.getBlockState(target);
+
+		Wandable wandable =
+				XplatAbstractions.INSTANCE.findWandable(
+						level,
+						target,
+						state,
+						level.getBlockEntity(target)
+				);
+
+		setSuccess(
+				wandable != null
+						&& wandable.onUsedByWand(
+								null,
+								stack,
+								facing.getOpposite()
+						)
+		);
+
 		return stack;
 	}
-
 }

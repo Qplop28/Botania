7d471e1dad2a814638e970e13bda4c77979f6fcf
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-13 00:36:15 -0700

    fix(minecraft): migrate phantom ink dispenser source

--- Xplat/src/main/java/vazkii/botania/common/block/dispenser/PhantomInkBehavior.java
+++ Xplat/src/main/java/vazkii/botania/common/block/dispenser/PhantomInkBehavior.java
@@ -1,8 +1,8 @@
 package vazkii.botania.common.block.dispenser;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.BlockSource;
 import net.minecraft.core.Direction;
+import net.minecraft.core.dispenser.BlockSource;
 import net.minecraft.core.dispenser.OptionalDispenseItemBehavior;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
@@ -14,16 +14,44 @@ import org.jetbrains.annotations.NotNull;
 import vazkii.botania.api.block.PhantomInkableBlock;
 import vazkii.botania.xplat.XplatAbstractions;
 
-public class PhantomInkBehavior extends OptionalDispenseItemBehavior {
+public class PhantomInkBehavior
+		extends OptionalDispenseItemBehavior {
 	@NotNull
 	@Override
-	protected ItemStack execute(BlockSource source, ItemStack stack) {
-		Level world = source.getLevel();
-		Direction facing = source.getBlockState().getValue(DispenserBlock.FACING);
-		BlockPos pos = source.getPos().relative(facing);
-		BlockState state = world.getBlockState(pos);
-		PhantomInkableBlock inkable = XplatAbstractions.INSTANCE.findPhantomInkable(world, pos, state, world.getBlockEntity(pos));
-		setSuccess(inkable != null && inkable.onPhantomInked(null, stack, facing.getOpposite()));
+	protected ItemStack execute(
+			BlockSource source,
+			ItemStack stack) {
+		Level level = source.level();
+
+		Direction facing =
+				source.state().getValue(
+						DispenserBlock.FACING
+				);
+
+		BlockPos target =
+				source.pos().relative(facing);
+
+		BlockState state =
+				level.getBlockState(target);
+
+		PhantomInkableBlock inkable =
+				XplatAbstractions.INSTANCE
+						.findPhantomInkable(
+								level,
+								target,
+								state,
+								level.getBlockEntity(target)
+						);
+
+		setSuccess(
+				inkable != null
+						&& inkable.onPhantomInked(
+								null,
+								stack,
+								facing.getOpposite()
+						)
+		);
+
 		return stack;
 	}
 }

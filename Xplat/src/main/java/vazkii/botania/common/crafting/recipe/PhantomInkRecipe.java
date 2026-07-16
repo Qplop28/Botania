f0ef30e97127eff4ed0f1ec5f3c1cf535b0b2f55
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-16 02:01:21 -0700

    fix(recipe): migrate phantom ink recipe

--- Xplat/src/main/java/vazkii/botania/common/crafting/recipe/PhantomInkRecipe.java
+++ Xplat/src/main/java/vazkii/botania/common/crafting/recipe/PhantomInkRecipe.java
@@ -8,78 +8,68 @@
  */
 package vazkii.botania.common.crafting.recipe;
 
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.resources.Identifier;
-import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
+import net.minecraft.world.item.crafting.CraftingInput;
 import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
-
-import org.jetbrains.annotations.NotNull;
 
 import vazkii.botania.api.item.PhantomInkable;
 import vazkii.botania.common.item.BotaniaItems;
 
 public class PhantomInkRecipe extends CustomRecipe {
-	public static final NoOpRecipeSerializer<PhantomInkRecipe> SERIALIZER = new NoOpRecipeSerializer<>(PhantomInkRecipe::new);
+	public static final RecipeSerializer<PhantomInkRecipe> SERIALIZER =
+			NoOpRecipeSerializer.create(PhantomInkRecipe::new);
 
-	public PhantomInkRecipe(Identifier id) {
-		super(id, CraftingBookCategory.EQUIPMENT);
+	@Override
+	public RecipeSerializer<PhantomInkRecipe> getSerializer() {
+		return SERIALIZER;
 	}
 
 	@Override
-	public boolean matches(@NotNull CraftingContainer var1, @NotNull Level var2) {
+	public CraftingBookCategory category() {
+		return CraftingBookCategory.EQUIPMENT;
+	}
+
+	@Override
+	public boolean matches(CraftingInput input, Level level) {
 		boolean foundInk = false;
 		boolean foundItem = false;
 
-		for (int i = 0; i < var1.getContainerSize(); i++) {
-			ItemStack stack = var1.getItem(i);
-			if (!stack.isEmpty()) {
-				if (stack.is(BotaniaItems.phantomInk) && !foundInk) {
-					foundInk = true;
-				} else if (!foundItem) {
-					if (stack.getItem() instanceof PhantomInkable && !stack.getItem().hasCraftingRemainingItem()) {
-						foundItem = true;
-					} else {
-						return false;
-					}
-				} else {
-					return false;
-				}
+		for (ItemStack stack : input.items()) {
+			if (stack.isEmpty()) {
+				continue;
+			}
+
+			if (stack.is(BotaniaItems.phantomInk) && !foundInk) {
+				foundInk = true;
+			} else if (!foundItem
+					&& stack.getItem() instanceof PhantomInkable
+					&& stack.getItem().getCraftingRemainder() == null) {
+				foundItem = true;
+			} else {
+				return false;
 			}
 		}
 
 		return foundInk && foundItem;
 	}
 
-	@NotNull
 	@Override
-	public ItemStack assemble(@NotNull CraftingContainer inv, @NotNull RegistryAccess registries) {
-		ItemStack item = ItemStack.EMPTY;
-
-		for (int i = 0; i < inv.getContainerSize(); i++) {
-			ItemStack stack = inv.getItem(i);
-			if (!stack.isEmpty() && stack.getItem() instanceof PhantomInkable && item.isEmpty()) {
-				item = stack;
+	public ItemStack assemble(CraftingInput input) {
+		for (ItemStack stack : input.items()) {
+			if (!stack.isEmpty()
+					&& stack.getItem() instanceof PhantomInkable inkable) {
+				ItemStack result = stack.copyWithCount(1);
+				inkable.setPhantomInk(
+						result,
+						!inkable.hasPhantomInk(stack)
+				);
+				return result;
 			}
 		}
 
-		PhantomInkable inkable = (PhantomInkable) item.getItem();
-		ItemStack copy = item.copy();
-		inkable.setPhantomInk(copy, !inkable.hasPhantomInk(item));
-		return copy;
-	}
-
-	@Override
-	public boolean canCraftInDimensions(int width, int height) {
-		return width * height >= 2;
-	}
-
-	@NotNull
-	@Override
-	public RecipeSerializer<?> getSerializer() {
-		return SERIALIZER;
+		return ItemStack.EMPTY;
 	}
 }

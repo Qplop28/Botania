900c2fa7c2ef5def2ed0f66d8246a7e17f4127a9
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-16 02:31:20 -0700

    fix(recipe): migrate ancient will recipe

--- Xplat/src/main/java/vazkii/botania/common/crafting/recipe/AncientWillRecipe.java
+++ Xplat/src/main/java/vazkii/botania/common/crafting/recipe/AncientWillRecipe.java
@@ -8,89 +8,100 @@
  */
 package vazkii.botania.common.crafting.recipe;
 
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.resources.Identifier;
-import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
+import net.minecraft.world.item.crafting.CraftingInput;
 import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
-
-import org.jetbrains.annotations.NotNull;
 
 import vazkii.botania.api.item.AncientWillContainer;
 import vazkii.botania.common.item.AncientWillItem;
 
 public class AncientWillRecipe extends CustomRecipe {
-	public static final NoOpRecipeSerializer<AncientWillRecipe> SERIALIZER = new NoOpRecipeSerializer<>(AncientWillRecipe::new);
+	public static final RecipeSerializer<AncientWillRecipe> SERIALIZER =
+			NoOpRecipeSerializer.create(AncientWillRecipe::new);
 
-	public AncientWillRecipe(Identifier id) {
-		super(id, CraftingBookCategory.EQUIPMENT);
+	@Override
+	public RecipeSerializer<AncientWillRecipe> getSerializer() {
+		return SERIALIZER;
 	}
 
 	@Override
-	public boolean matches(@NotNull CraftingContainer inv, @NotNull Level world) {
-		boolean foundWill = false;
-		boolean foundItem = false;
+	public CraftingBookCategory category() {
+		return CraftingBookCategory.EQUIPMENT;
+	}
 
-		for (int i = 0; i < inv.getContainerSize(); i++) {
-			ItemStack stack = inv.getItem(i);
-			if (!stack.isEmpty()) {
-				if (stack.getItem() instanceof AncientWillItem) {
-					if (foundWill) {
-						return false;
-					}
-					foundWill = true;
-				} else if (stack.getItem() instanceof AncientWillContainer) {
-					if (foundItem) {
-						return false;
-					}
-					foundItem = true;
-				} else {
+	@Override
+	public boolean matches(CraftingInput input, Level level) {
+		boolean foundWill = false;
+		boolean foundContainer = false;
+
+		for (ItemStack stack : input.items()) {
+			if (stack.isEmpty()) {
+				continue;
+			}
+
+			if (stack.getItem() instanceof AncientWillItem) {
+				if (foundWill) {
 					return false;
 				}
+
+				foundWill = true;
+			} else if (stack.getItem() instanceof AncientWillContainer) {
+				if (foundContainer) {
+					return false;
+				}
+
+				foundContainer = true;
+			} else {
+				return false;
 			}
 		}
 
-		return foundWill && foundItem;
+		return foundWill && foundContainer;
 	}
 
-	@NotNull
 	@Override
-	public ItemStack assemble(@NotNull CraftingContainer inv, @NotNull RegistryAccess registries) {
-		ItemStack item = ItemStack.EMPTY;
+	public ItemStack assemble(CraftingInput input) {
+		ItemStack containerStack = ItemStack.EMPTY;
 		AncientWillContainer.AncientWillType will = null;
 
-		for (int i = 0; i < inv.getContainerSize(); i++) {
-			ItemStack stack = inv.getItem(i);
-			if (!stack.isEmpty()) {
-				if (stack.getItem() instanceof AncientWillContainer && item.isEmpty()) {
-					item = stack;
-				} else {
-					will = ((AncientWillItem) stack.getItem()).type; // we already verified this is a will in matches()
+		for (ItemStack stack : input.items()) {
+			if (stack.isEmpty()) {
+				continue;
+			}
+
+			if (stack.getItem() instanceof AncientWillItem willItem) {
+				if (will != null) {
+					return ItemStack.EMPTY;
 				}
+
+				will = willItem.type;
+			} else if (stack.getItem() instanceof AncientWillContainer) {
+				if (!containerStack.isEmpty()) {
+					return ItemStack.EMPTY;
+				}
+
+				containerStack = stack;
+			} else {
+				return ItemStack.EMPTY;
 			}
 		}
 
-		AncientWillContainer container = (AncientWillContainer) item.getItem();
-		if (container.hasAncientWill(item, will)) {
+		if (containerStack.isEmpty() || will == null) {
 			return ItemStack.EMPTY;
 		}
 
-		ItemStack copy = item.copy();
-		container.addAncientWill(copy, will);
-		return copy;
-	}
+		AncientWillContainer container =
+				(AncientWillContainer) containerStack.getItem();
 
-	@Override
-	public boolean canCraftInDimensions(int width, int height) {
-		return width > 1 || height > 1;
-	}
+		if (container.hasAncientWill(containerStack, will)) {
+			return ItemStack.EMPTY;
+		}
 
-	@NotNull
-	@Override
-	public RecipeSerializer<?> getSerializer() {
-		return SERIALIZER;
+		ItemStack result = containerStack.copyWithCount(1);
+		container.addAncientWill(result, will);
+		return result;
 	}
 }

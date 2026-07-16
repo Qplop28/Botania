4c6152fad60f7e937c2fedf98c6f958e16bfd6df
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-16 14:35:51 -0700

    fix(recipe): migrate wand of the forest serializer

--- Xplat/src/main/java/vazkii/botania/common/crafting/recipe/WandOfTheForestRecipe.java
+++ Xplat/src/main/java/vazkii/botania/common/crafting/recipe/WandOfTheForestRecipe.java
@@ -8,82 +8,142 @@
  */
 package vazkii.botania.common.crafting.recipe;
 
-import com.google.gson.JsonObject;
+import com.mojang.serialization.MapCodec;
 
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.resources.Identifier;
-import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.network.RegistryFriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.CraftingBookCategory;
+import net.minecraft.world.item.crafting.CraftingInput;
+import net.minecraft.world.item.crafting.CraftingRecipe;
+import net.minecraft.world.item.crafting.PlacementInfo;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.item.crafting.ShapedRecipe;
-
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.world.item.crafting.display.RecipeDisplay;
+import net.minecraft.world.level.Level;
 
 import vazkii.botania.common.block.decor.BotaniaMushroomBlock;
 import vazkii.botania.common.item.WandOfTheForestItem;
 import vazkii.botania.common.item.material.MysticalPetalItem;
 
-public class WandOfTheForestRecipe extends ShapedRecipe {
-	public static final RecipeSerializer<WandOfTheForestRecipe> SERIALIZER = new Serializer();
+import java.util.List;
 
-	public WandOfTheForestRecipe(ShapedRecipe compose) {
-		super(compose.getId(), compose.getGroup(), compose.category(), compose.getWidth(), compose.getHeight(),
-				compose.getIngredients(),
-				// XXX: Hacky, but compose should always be a vanilla shaped recipe which doesn't do anything with the
-				// RegistryAccess
-				compose.getResultItem(RegistryAccess.EMPTY));
+public final class WandOfTheForestRecipe implements CraftingRecipe {
+	private static final MapCodec<WandOfTheForestRecipe> CODEC =
+			ShapedRecipe.MAP_CODEC.xmap(
+					WandOfTheForestRecipe::new,
+					WandOfTheForestRecipe::delegate
+			);
+
+	private static final StreamCodec<
+			RegistryFriendlyByteBuf,
+			WandOfTheForestRecipe
+	> STREAM_CODEC = new StreamCodec<>() {
+		@Override
+		public WandOfTheForestRecipe decode(
+				RegistryFriendlyByteBuf buffer
+		) {
+			return new WandOfTheForestRecipe(
+					ShapedRecipe.STREAM_CODEC.decode(buffer)
+			);
+		}
+
+		@Override
+		public void encode(
+				RegistryFriendlyByteBuf buffer,
+				WandOfTheForestRecipe recipe
+		) {
+			ShapedRecipe.STREAM_CODEC.encode(
+					buffer,
+					recipe.delegate
+			);
+		}
+	};
+
+	public static final RecipeSerializer<WandOfTheForestRecipe> SERIALIZER =
+			new RecipeSerializer<>(CODEC, STREAM_CODEC);
+
+	private final ShapedRecipe delegate;
+
+	public WandOfTheForestRecipe(ShapedRecipe delegate) {
+		this.delegate = delegate;
 	}
 
-	@NotNull
-	@Override
-	public ItemStack assemble(CraftingContainer inv, @NotNull RegistryAccess registries) {
-		int first = -1;
-		for (int i = 0; i < inv.getContainerSize(); i++) {
-			ItemStack stack = inv.getItem(i);
-			Item item = stack.getItem();
+	private ShapedRecipe delegate() {
+		return this.delegate;
+	}
 
-			int colorId;
+	@Override
+	public boolean matches(CraftingInput input, Level level) {
+		return this.delegate.matches(input, level);
+	}
+
+	@Override
+	public ItemStack assemble(CraftingInput input) {
+		ItemStack result = this.delegate.assemble(input);
+		int firstColor = -1;
+
+		for (ItemStack stack : input.items()) {
+			Item item = stack.getItem();
+			int color;
+
 			if (item instanceof MysticalPetalItem petal) {
-				colorId = petal.color.getId();
-			} else if (item instanceof BlockItem block && block.getBlock() instanceof BotaniaMushroomBlock mushroom) {
-				colorId = mushroom.color.getId();
+				color = petal.color.getId();
+			} else if (item instanceof BlockItem blockItem
+					&& blockItem.getBlock()
+							instanceof BotaniaMushroomBlock mushroom) {
+				color = mushroom.color.getId();
 			} else {
 				continue;
 			}
-			if (first == -1) {
-				first = colorId;
+
+			if (firstColor == -1) {
+				firstColor = color;
 			} else {
-				return WandOfTheForestItem.setColors(getResultItem(registries).copy(), first, colorId);
+				return WandOfTheForestItem.setColors(
+						result,
+						firstColor,
+						color
+				);
 			}
 		}
-		return WandOfTheForestItem.setColors(getResultItem(registries).copy(), first != -1 ? first : 0, 0);
+
+		return WandOfTheForestItem.setColors(
+				result,
+				firstColor != -1 ? firstColor : 0,
+				0
+		);
 	}
 
-	@NotNull
 	@Override
-	public RecipeSerializer<?> getSerializer() {
+	public boolean showNotification() {
+		return this.delegate.showNotification();
+	}
+
+	@Override
+	public String group() {
+		return this.delegate.group();
+	}
+
+	@Override
+	public RecipeSerializer<WandOfTheForestRecipe> getSerializer() {
 		return SERIALIZER;
 	}
 
-	private static class Serializer implements RecipeSerializer<WandOfTheForestRecipe> {
-		@NotNull
-		@Override
-		public WandOfTheForestRecipe fromJson(@NotNull Identifier recipeId, @NotNull JsonObject json) {
-			return new WandOfTheForestRecipe(SHAPED_RECIPE.fromJson(recipeId, json));
-		}
+	@Override
+	public PlacementInfo placementInfo() {
+		return this.delegate.placementInfo();
+	}
 
-		@NotNull
-		@Override
-		public WandOfTheForestRecipe fromNetwork(@NotNull Identifier recipeId, @NotNull FriendlyByteBuf buffer) {
-			return new WandOfTheForestRecipe(SHAPED_RECIPE.fromNetwork(recipeId, buffer));
-		}
+	@Override
+	public CraftingBookCategory category() {
+		return this.delegate.category();
+	}
 
-		@Override
-		public void toNetwork(@NotNull FriendlyByteBuf buffer, @NotNull WandOfTheForestRecipe recipe) {
-			SHAPED_RECIPE.toNetwork(buffer, recipe);
-		}
+	@Override
+	public List<RecipeDisplay> display() {
+		return this.delegate.display();
 	}
 }

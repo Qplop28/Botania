95d48a35b1255e14ab5c26d25374c505f8fd4af9
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-16 02:17:32 -0700

    fix(recipe): migrate mana blaster lens recipe

--- Xplat/src/main/java/vazkii/botania/common/crafting/recipe/ManaBlasterLensRecipe.java
+++ Xplat/src/main/java/vazkii/botania/common/crafting/recipe/ManaBlasterLensRecipe.java
@@ -1,69 +1,75 @@
 /*
  * This class is distributed as part of the Botania Mod.
  * Get the Source Code in github:
- * https://github.com/Vazkii/Botania
+ * https://github.com/VazkiiMods/Botania
  *
  * Botania is Open Source and distributed under the
  * Botania License: http://botaniamod.net/license.php
  */
 package vazkii.botania.common.crafting.recipe;
 
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.resources.Identifier;
-import net.minecraft.world.inventory.CraftingContainer;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
+import net.minecraft.world.item.crafting.CraftingInput;
 import net.minecraft.world.item.crafting.CustomRecipe;
 import net.minecraft.world.item.crafting.RecipeSerializer;
 import net.minecraft.world.level.Level;
 
-import org.jetbrains.annotations.NotNull;
-
-import vazkii.botania.api.mana.BasicLensItem;
 import vazkii.botania.common.item.ManaBlasterItem;
 
 public class ManaBlasterLensRecipe extends CustomRecipe {
-	public static final NoOpRecipeSerializer<ManaBlasterLensRecipe> SERIALIZER = new NoOpRecipeSerializer<>(ManaBlasterLensRecipe::new);
+	public static final RecipeSerializer<ManaBlasterLensRecipe> SERIALIZER =
+			NoOpRecipeSerializer.create(ManaBlasterLensRecipe::new);
 
-	public ManaBlasterLensRecipe(Identifier id) {
-		super(id, CraftingBookCategory.EQUIPMENT);
+	@Override
+	public RecipeSerializer<ManaBlasterLensRecipe> getSerializer() {
+		return SERIALIZER;
 	}
 
 	@Override
-	public boolean matches(@NotNull CraftingContainer inv, @NotNull Level world) {
-		int foundLens = 0;
-		int foundGun = 0;
+	public CraftingBookCategory category() {
+		return CraftingBookCategory.EQUIPMENT;
+	}
 
-		for (int i = 0; i < inv.getContainerSize(); i++) {
-			ItemStack stack = inv.getItem(i);
-			if (!stack.isEmpty()) {
-				if (stack.getItem() instanceof ManaBlasterItem && ManaBlasterItem.getLens(stack).isEmpty()) {
-					foundGun++;
-				} else if (ManaBlasterItem.isValidLens(stack)) {
-					foundLens++;
-				} else {
-					return false; // Found an invalid item, breaking the recipe
-				}
+	@Override
+	public boolean matches(CraftingInput input, Level level) {
+		boolean foundLens = false;
+		boolean foundGun = false;
+
+		for (ItemStack stack : input.items()) {
+			if (stack.isEmpty()) {
+				continue;
+			}
+
+			if (stack.getItem() instanceof ManaBlasterItem
+					&& ManaBlasterItem.getLens(stack).isEmpty()
+					&& !foundGun) {
+				foundGun = true;
+			} else if (ManaBlasterItem.isValidLens(stack)
+					&& !foundLens) {
+				foundLens = true;
+			} else {
+				return false;
 			}
 		}
 
-		return foundLens == 1 && foundGun == 1;
+		return foundLens && foundGun;
 	}
 
-	@NotNull
 	@Override
-	public ItemStack assemble(@NotNull CraftingContainer inv, @NotNull RegistryAccess registries) {
+	public ItemStack assemble(CraftingInput input) {
 		ItemStack lens = ItemStack.EMPTY;
 		ItemStack gun = ItemStack.EMPTY;
 
-		for (int i = 0; i < inv.getContainerSize(); i++) {
-			ItemStack stack = inv.getItem(i);
-			if (!stack.isEmpty()) {
-				if (stack.getItem() instanceof ManaBlasterItem) {
-					gun = stack;
-				} else if (stack.getItem() instanceof BasicLensItem) {
-					lens = stack.copyWithCount(1);
-				}
+		for (ItemStack stack : input.items()) {
+			if (stack.isEmpty()) {
+				continue;
+			}
+
+			if (stack.getItem() instanceof ManaBlasterItem) {
+				gun = stack;
+			} else if (ManaBlasterItem.isValidLens(stack)) {
+				lens = stack.copyWithCount(1);
 			}
 		}
 
@@ -71,20 +77,8 @@ public class ManaBlasterLensRecipe extends CustomRecipe {
 			return ItemStack.EMPTY;
 		}
 
-		ItemStack gunCopy = gun.copy();
-		ManaBlasterItem.setLens(gunCopy, lens);
-
-		return gunCopy;
-	}
-
-	@Override
-	public boolean canCraftInDimensions(int width, int height) {
-		return width * height >= 2;
-	}
-
-	@NotNull
-	@Override
-	public RecipeSerializer<?> getSerializer() {
-		return SERIALIZER;
+		ItemStack result = gun.copyWithCount(1);
+		ManaBlasterItem.setLens(result, lens);
+		return result;
 	}
 }

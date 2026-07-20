503d09ebccd10a0cce1bba17478fa80c711e5263
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-19 20:39:47 -0700

    fix(platform): correct tooltip display import

--- Xplat/src/main/java/vazkii/botania/common/block/BotaniaBlocks.java
+++ Xplat/src/main/java/vazkii/botania/common/block/BotaniaBlocks.java
@@ -28,7 +28,7 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.StandingAndWallBlockItem;
-import net.minecraft.world.item.TooltipDisplay;
+import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.*;

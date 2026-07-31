d0d0d88eba567da604a4fcf31ac2aca15b66d7e7
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-07-31 10:11:51 -0700

    fix(bauble): register deferred accessory renderers

--- Xplat/src/main/java/vazkii/botania/client/render/AccessoryRenderRegistry.java
+++ Xplat/src/main/java/vazkii/botania/client/render/AccessoryRenderRegistry.java
@@ -5,11 +5,14 @@ import net.minecraft.world.item.ItemStack;
 
 import org.jetbrains.annotations.Nullable;
 
+import vazkii.botania.client.render.accessory.DeferredAccessoryRenderer;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class AccessoryRenderRegistry {
 	private static final Map<Item, AccessoryRenderer> REGISTRATIONS = new HashMap<>();
+	private static final Map<Item, DeferredAccessoryRenderer> DEFERRED_REGISTRATIONS = new HashMap<>();
 
 	public static void register(Item item, AccessoryRenderer renderer) {
 		REGISTRATIONS.put(item, renderer);
@@ -18,5 +21,14 @@ public class AccessoryRenderRegistry {
 	@Nullable
 	public static AccessoryRenderer get(ItemStack stack) {
 		return REGISTRATIONS.get(stack.getItem());
+	}
+
+	public static void registerDeferred(Item item, DeferredAccessoryRenderer renderer) {
+		DEFERRED_REGISTRATIONS.put(item, renderer);
+	}
+
+	@Nullable
+	public static DeferredAccessoryRenderer getDeferred(ItemStack stack) {
+		return DEFERRED_REGISTRATIONS.get(stack.getItem());
 	}
 }

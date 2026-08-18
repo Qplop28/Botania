844f0665187d5a99a6a47c26a614ebae70b04568
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-08-18 16:05:00 -0700

    fix(gui): finish 26.1.2 container screen migration (#114)

--- Xplat/src/main/java/vazkii/botania/client/gui/bag/FlowerPouchGui.java
+++ Xplat/src/main/java/vazkii/botania/client/gui/bag/FlowerPouchGui.java
@@ -8,11 +8,10 @@
  */
 package vazkii.botania.client.gui.bag;
 
-import com.mojang.blaze3d.pipeline.RenderPipelines;
-
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Inventory;
@@ -29,13 +28,18 @@ public class FlowerPouchGui extends AbstractContainerScreen<FlowerPouchContainer
 	}
 
 	@Override
-	protected void renderBg(GuiGraphicsExtractor gui, int mouseX, int mouseY) {
-		var pose = gui.pose();
-		Minecraft mc = Minecraft.getInstance();
+	protected void extractBackground(GuiGraphicsExtractor gui, float partialTick, int mouseX, int mouseY) {
 		int k = (width - imageWidth) / 2;
 		int l = (height - imageHeight) / 2;
 		gui.blit(RenderPipelines.GUI_TEXTURED, texture, k, l, 0, 0,
 				imageWidth, imageHeight, imageWidth, imageHeight, 256, 256, 0xFFFFFFFF);
+	}
+
+	@Override
+	public void extractRenderState(GuiGraphicsExtractor gui, int mouseX, int mouseY, float partialTick) {
+		super.extractRenderState(gui, mouseX, mouseY, partialTick);
+		Minecraft mc = Minecraft.getInstance();
+		gui.nextStratum();
 
 		for (Slot slot : menu.slots) {
 			if (slot.container == menu.flowerBagInv
@@ -45,9 +49,6 @@ public class FlowerPouchGui extends AbstractContainerScreen<FlowerPouchContainer
 				int y = this.topPos + slot.y;
 
 				// Always draw the count even at 1
-				pose.pushMatrix();
-				// Same as how much vanilla offsets when drawing items in GUIs
-				pose.translate(0, 0, 200);
 				gui.text(
 						mc.font,
 						"1",
@@ -56,7 +57,6 @@ public class FlowerPouchGui extends AbstractContainerScreen<FlowerPouchContainer
 						0xFFFFFF,
 						true
 				);
-				pose.popMatrix();
 			}
 		}
 	}

b6f42738ca632c21b3627b2445bae7e3a8a86476
Qplop28 <132115806+Qplop28@users.noreply.github.com>
2026-08-19 01:17:10 -0700

    fix(block-entities): finish residual 26.1.2 APIs

--- Xplat/src/main/java/vazkii/botania/common/block/block_entity/CacophoniumBlockEntity.java
+++ Xplat/src/main/java/vazkii/botania/common/block/block_entity/CacophoniumBlockEntity.java
@@ -9,13 +9,18 @@
 package vazkii.botania.common.block.block_entity;
 
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.resources.RegistryOps;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
 
 import vazkii.botania.common.item.CacophoniumItem;
 
@@ -30,30 +35,39 @@ public class CacophoniumBlockEntity extends BotaniaBlockEntity {
 
 	public void annoyDirewolf() {
 		CacophoniumItem.playSound(level, stack, worldPosition.getX(), worldPosition.getY(), worldPosition.getZ(), SoundSource.BLOCKS, 1F);
-		if (!level.isClientSide) {
-			float noteColor = level.random.nextInt(25) / 24.0F;
+		if (!level.isClientSide()) {
+			float noteColor = level.getRandom().nextInt(25) / 24.0F;
 			((ServerLevel) level).sendParticles(ParticleTypes.NOTE, worldPosition.getX() + 0.5, worldPosition.getY() + 1.2, worldPosition.getZ() + 0.5, 0, noteColor, 0, 0, 1);
 			level.gameEvent(null, GameEvent.NOTE_BLOCK_PLAY, getBlockPos());
 		}
 	}
 
 	@Override
-	public void writePacketNBT(CompoundTag cmp) {
-		super.writePacketNBT(cmp);
-
-		CompoundTag cmp1 = new CompoundTag();
-		if (!stack.isEmpty()) {
-			cmp1 = stack.save(cmp1);
-		}
-		cmp.put(TAG_STACK, cmp1);
+	protected void writePacketNBT(CompoundTag cmp, HolderLookup.Provider registryLookup) {
+		ItemStack.OPTIONAL_CODEC.encodeStart(RegistryOps.create(NbtOps.INSTANCE, registryLookup), stack)
+				.result().ifPresent(encoded -> cmp.put(TAG_STACK, encoded));
 	}
 
 	@Override
-	public void readPacketNBT(CompoundTag cmp) {
-		super.readPacketNBT(cmp);
+	protected void readPacketNBT(CompoundTag cmp, HolderLookup.Provider registryLookup) {
+		stack = cmp.get(TAG_STACK) == null ? ItemStack.EMPTY
+				: ItemStack.OPTIONAL_CODEC.parse(RegistryOps.create(NbtOps.INSTANCE, registryLookup), cmp.get(TAG_STACK))
+						.result().orElse(ItemStack.EMPTY);
+	}
 
-		CompoundTag cmp1 = cmp.getCompound(TAG_STACK);
-		stack = ItemStack.of(cmp1);
+	@Override
+	protected void writePersistentData(ValueOutput output) {
+		output.store(TAG_STACK, ItemStack.OPTIONAL_CODEC, stack);
+	}
+
+	@Override
+	protected void readPersistentData(ValueInput input) {
+		input.read(TAG_STACK, ItemStack.OPTIONAL_CODEC).ifPresent(value -> stack = value);
+	}
+
+	@Override
+	protected void readNestedLegacyPersistentData(CompoundTag tag, HolderLookup.Provider registryLookup) {
+		readPacketNBT(tag, registryLookup);
 	}
 
 }
